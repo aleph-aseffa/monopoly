03c7f23fe57079a86cd0157641d8e4ee596726e6
aleph <aseffaal@grinnell.edu>
2019-07-23 15:22:39 +0300

    Added docstrings to each function.

--- initialization.py
+++ initialization.py
@@ -4,8 +4,10 @@ Date created: 7/14/2019
 
 Contains all the functions needed for the initialization of the game.
 
+
+Functions that will be included:
+- print header
+- ask how many players will be playing
+- for each one, take their username
+
 """
-
-
-
-
