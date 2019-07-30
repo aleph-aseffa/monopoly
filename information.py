45f9054f2120fd699a1094de52a067e8049e433b
aleph <43659139+aleph-aseffa@users.noreply.github.com>
2019-07-30 10:52:09 +0300

    Added card data for purchasable properties. Resolves #4

--- information.py
+++ information.py
@@ -19,50 +19,160 @@ def initialize_cards_and_board():
     The board represents the order of cards in the original Monopoly game board.
     :return: board: a list of cards.
     """
-    go = c_def.Card("Go", "N/A", 0, 0, 0, {}, 0, "Bank", False)
-    med_ave = c_def.Card("Mediterranean Avenue", "Brown", 60, 0, 0, {1: 100}, 0, "Bank", False)
-    comm_chest = c_def.Card("Community Chest", "N/A", 0, 0, 0, {1: 100}, 0, "Bank", False)
-    baltic_ave = c_def.Card("Baltic Avenue", "Brown", 60, 0, 0, {1: 100}, 0, "Bank", False)
+    go = c_def.Card("Go", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "Bank", False)
+    med_ave = c_def.Card("Mediterranean Avenue", "Brown", 60, 50, 0, {0: 2,
+                                                                      1: 10,
+                                                                      2: 30,
+                                                                      3: 90,
+                                                                      4: 160,
+                                                                      5: 250}, 30, "Bank", False)
+    comm_chest = c_def.Card("Community Chest", "N/A", 0, 0, 0, "N/A", 0, "Bank", False)
+    baltic_ave = c_def.Card("Baltic Avenue", "Brown", 60, 50, 0, {0: 4,
+                                                                  1: 20,
+                                                                  2: 60,
+                                                                  3: 180,
+                                                                  4: 320,
+                                                                  5: 450}, 30, "Bank", False)
     income_tax = c_def.Card("Income Tax", "N/A", 200, 0, 0, {1: 100}, 0, "Bank", False)
-    reading_rr = c_def.Card("Reading Railroad", "Railroad", 200, 0, 0, {1: 100}, 0, "Bank", False)
-    oriental_ave = c_def.Card("Oriental Avenue", "Light Blue", 100, 0, 0, {1: 100}, 0, "Bank", False)
+    reading_rr = c_def.Card("Reading Railroad", "Railroad", 200, "N/A", "N/A", {1: 25}, 100, "Bank", False)
+    oriental_ave = c_def.Card("Oriental Avenue", "Light Blue", 100, 50, 0, {0: 6,
+                                                                            1: 30,
+                                                                            2: 90,
+                                                                            3: 270,
+                                                                            4: 400,
+                                                                            5: 550}, 50, "Bank", False)
     chance = c_def.Card("Community Chest", "N/A", 0, 0, 0, {1: 100}, 0, "Bank", False)
-    vermont_ave = c_def.Card("Vermont Avenue", "Light Blue", 100, 0, 0, {1: 100}, 0, "Bank", False)
-    conn_ave = c_def.Card("Connecticut Avenue", "Light Blue", 120, 0, 0, {1: 100}, 0, "Bank", False)
-
+    vermont_ave = c_def.Card("Vermont Avenue", "Light Blue", 100, 50, 0, {0: 6,
+                                                                          1: 30,
+                                                                          2: 90,
+                                                                          3: 270,
+                                                                          4: 400,
+                                                                          5: 550}, 50, "Bank", False)
+    conn_ave = c_def.Card("Connecticut Avenue", "Light Blue", 120, 50, 0, {0: 8,
+                                                                           1: 40,
+                                                                           2: 100,
+                                                                           3: 300,
+                                                                           4: 450,
+                                                                           5: 600}, 60, "Bank", False)
+    # new row
     jail = c_def.Card("Jail/Visiting Jail", "N/A", 0, 0, 0, {1: 100}, 0, "Bank", False)
-    st_charles_place = c_def.Card("St. Charles Place", "Pink", 140, 0, 0, {1: 100}, 0, "Bank", False)
-    electric_company = c_def.Card("Electric Company", "Utilities", 150, 0, 0, {1: 100}, 0, "Bank", False)
-    states_ave = c_def.Card("States Avenue", "Pink", 140, 0, 0, {1: 100}, 0, "Bank", False)
-    virginia_ave = c_def.Card("Virginia Avenue", "Pink", 160, 0, 0, {1: 100}, 0, "Bank", False)
-    penn_rr = c_def.Card("Pennsylvania Railroad", "Railroad", 200, 0, 0, {1: 100}, 0, "Bank", False)
-    st_james_place = c_def.Card("St. James Place", "Orange", 180, 0, 0, {1: 100}, 0, "Bank", False)
+    st_charles_place = c_def.Card("St. Charles Place", "Pink", 140, 100, 0, {0: 10,
+                                                                             1: 50,
+                                                                             2: 150,
+                                                                             3: 450,
+                                                                             4: 625,
+                                                                             5: 750}, 70, "Bank", False)
+    electric_company = c_def.Card("Electric Company", "Utilities", 150, "N/A", "N/A", "N/A", 75, "Bank", False)
+    states_ave = c_def.Card("States Avenue", "Pink", 140, 100, 0, {0: 10,
+                                                                   1: 50,
+                                                                   2: 150,
+                                                                   3: 450,
+                                                                   4: 625,
+                                                                   5: 750}, 70, "Bank", False)
+    virginia_ave = c_def.Card("Virginia Avenue", "Pink", 160, 100, 0, {0: 12,
+                                                                       1: 60,
+                                                                       2: 180,
+                                                                       3: 500,
+                                                                       4: 700,
+                                                                       5: 900}, 80, "Bank", False)
+    penn_rr = c_def.Card("Pennsylvania Railroad", "Railroad", 200, 0, 0, {1: 100}, 100, "Bank", False)
+    st_james_place = c_def.Card("St. James Place", "Orange", 180, 100, 0, {0: 14,
+                                                                           1: 70,
+                                                                           2: 200,
+                                                                           3: 550,
+                                                                           4: 750,
+                                                                           5: 950}, 90, "Bank", False)
     # comm chest
-    ten_ave = c_def.Card("Tennessee Avenue", "Orange", 180, 0, 0, {1: 100}, 0, "Bank", False)
-    ny_ave = c_def.Card("New York Avenue", "Orange", 200, 0, 0, {1: 100}, 0, "Bank", False)
+    ten_ave = c_def.Card("Tennessee Avenue", "Orange", 180, 100, 0, {0: 14,
+                                                                     1: 70,
+                                                                     2: 200,
+                                                                     3: 550,
+                                                                     4: 750,
+                                                                     5: 950}, 90, "Bank", False)
+    ny_ave = c_def.Card("New York Avenue", "Orange", 200, 100, 0, {0: 16,
+                                                                   1: 80,
+                                                                   2: 220,
+                                                                   3: 600,
+                                                                   4: 800,
+                                                                   5: 1000}, 100, "Bank", False)
     free_parking = c_def.Card("Free Parking", "N/A", 0, 0, 0, {1: 100}, 0, "Bank", False)
-
-    kentucky_ave = c_def.Card("Kentucky Avenue", "Red", 220, 0, 0, {1: 100}, 0, "Bank", False)
+    # rotate
+    kentucky_ave = c_def.Card("Kentucky Avenue", "Red", 220, 150, 0, {0: 18,
+                                                                      1: 90,
+                                                                      2: 250,
+                                                                      3: 700,
+                                                                      4: 875,
+                                                                      5: 1050}, 110, "Bank", False)
     # chance
-    indiana_ave = c_def.Card("Indiana Avenue", "Red", 220, 0, 0, {1: 100}, 0, "Bank", False)
-    illinois_ave = c_def.Card("Illinois Avenue", "Red", 240, 0, 0, {1: 100}, 0, "Bank", False)
-    bno_rr = c_def.Card("B. & O. Railroad", "Railroad", 200, 0, 0, {1: 100}, 0, "Bank", False)
-    atlantic_ave = c_def.Card("Atlantic Avenue", "Yellow", 260, 0, 0, {1: 100}, 0, "Bank", False)
-    ventnor_ave = c_def.Card("Ventnor Avenue", "Yellow", 260, 0, 0, {1: 100}, 0, "Bank", False)
-    water_works = c_def.Card("Water Works", "Utilities", 150, 0, 0, {1: 100}, 0, "Bank", False)
-    marvin_gardens = c_def.Card("Marvin Gardens", "Yellow", 280, 0, 0, {1: 100}, 0, "Bank", False)
-
+    indiana_ave = c_def.Card("Indiana Avenue", "Red", 220, 150, 0, {0: 18,
+                                                                    1: 90,
+                                                                    2: 250,
+                                                                    3: 700,
+                                                                    4: 875,
+                                                                    5: 1050}, 110, "Bank", False)
+    illinois_ave = c_def.Card("Illinois Avenue", "Red", 240, 150, 0, {0: 20,
+                                                                      1: 100,
+                                                                      2: 300,
+                                                                      3: 750,
+                                                                      4: 925,
+                                                                      5: 1100}, 120, "Bank", False)
+    bno_rr = c_def.Card("B. & O. Railroad", "Railroad", 200, 0, 0, {1: 100}, 100, "Bank", False)
+    atlantic_ave = c_def.Card("Atlantic Avenue", "Yellow", 260, 150, 0, {0: 22,
+                                                                         1: 110,
+                                                                         2: 330,
+                                                                         3: 800,
+                                                                         4: 975,
+                                                                         5: 1150}, 130, "Bank", False)
+    ventnor_ave = c_def.Card("Ventnor Avenue", "Yellow", 260, 150, 0, {0: 22,
+                                                                       1: 110,
+                                                                       2: 330,
+                                                                       3: 800,
+                                                                       4: 975,
+                                                                       5: 1150}, 130, "Bank", False)
+    water_works = c_def.Card("Water Works", "Utilities", 150, 0, 0, {1: 100}, 75, "Bank", False)
+    marvin_gardens = c_def.Card("Marvin Gardens", "Yellow", 280, 150, 0, {0: 24,
+                                                                          1: 120,
+                                                                          2: 360,
+                                                                          3: 850,
+                                                                          4: 1025,
+                                                                          5: 1200}, 140, "Bank", False)
+    # rotate
     go_to_jail = c_def.Card("Go to Jail", "N/A", 0, 0, 0, {1: 100}, 0, "Bank", False)
-    pacific_ave = c_def.Card("Pacific Avenue", "Green", 300, 0, 0, {1: 100}, 0, "Bank", False)
-    nc_ave = c_def.Card("North Carolina Avenue", "Green", 140, 0, 0, {1: 100}, 0, "Bank", False)
+    pacific_ave = c_def.Card("Pacific Avenue", "Green", 300, 200, 0, {0: 26,
+                                                                      1: 130,
+                                                                      2: 390,
+                                                                      3: 900,
+                                                                      4: 1100,
+                                                                      5: 1275}, 150, "Bank", False)
+    nc_ave = c_def.Card("North Carolina Avenue", "Green", 140, 200, 0, {0: 26,
+                                                                        1: 130,
+                                                                        2: 390,
+                                                                        3: 900,
+                                                                        4: 1100,
+                                                                        5: 1275}, 150, "Bank", False)
     # comm chest
-    penn_ave = c_def.Card("Pennsylvania Avenue", "Green", 300, 0, 0, {1: 100}, 0, "Bank", False)
-    short_line_rr = c_def.Card("Short Line", "Railroad", 200, 0, 0, {1: 100}, 0, "Bank", False)
+    penn_ave = c_def.Card("Pennsylvania Avenue", "Green", 300, 200, 0, {0: 28,
+                                                                        1: 150,
+                                                                        2: 450,
+                                                                        3: 1000,
+                                                                        4: 1200,
+                                                                        5: 1400}, 160, "Bank", False)
+    short_line_rr = c_def.Card("Short Line", "Railroad", 200, 0, 0, {1: 100}, 100, "Bank", False)
     # chance
-    park_place = c_def.Card("Park Place", "Blue", 350, 0, 0, {1: 100}, 0, "Bank", False)
+    park_place = c_def.Card("Park Place", "Blue", 350, 200, 0, {0: 35,
+                                                                1: 175,
+                                                                2: 500,
+                                                                3: 1100,
+                                                                4: 1300,
+                                                                5: 1500}, 175, "Bank", False)
     luxury_tax = c_def.Card("Luxury Tax", "N/A", 100, 0, 0, {1: 100}, 0, "Bank", False)
-    boardwalk = c_def.Card("Boardwalk", "N/A", 400, 0, 0, {1: 100}, 0, "Bank", False)
-
+    boardwalk = c_def.Card("Boardwalk", "N/A", 400, 200, 0, {0: 50,
+                                                             1: 200,
+                                                             2: 600,
+                                                             3: 1400,
+                                                             4: 1700,
+                                                             5: 2000}, 200, "Bank", False)
+    # end
     board = [
         go,
         med_ave,
